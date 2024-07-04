ca2dae739cf8d2c589389a9d3872bd0cc9f278af
typhoon0678 <typhoon0678@gmail.com>
2024-07-04 22:04:07 +0900

    typhoon0678 step0

---
+++ src/main/java/org/homework/InputMenu.java
@@ -0,0 +1,36 @@
+package org.homework;
+
+import java.util.Arrays;
+
+public enum InputMenu {
+    ADD("1"),
+    DELETE("2"),
+    VIEW("3"),
+    EXIT("4"),
+    OTHERWISE("");
+
+    private final String input;
+
+    InputMenu(String input) {
+        this.input = input;
+    }
+
+    public static InputMenu getInputMenu(String input) {
+
+        return Arrays.stream(values())
+                .filter(inputMenu -> inputMenu.input.equals(input))
+                .findAny()
+                .orElse(OTHERWISE);
+    }
+
+//    public static InputMenu getInputMenu(String input) {
+//
+//        for (InputMenu inputMenu : values()) {
+//            if (inputMenu.input.equals(input)) {
+//                return inputMenu;
+//            }
+//        }
+//
+//        return OTHERWISE;
+//    }
+}
--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -1,7 +1,16 @@
 package org.homework;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        InputMenu inputMenu = InputMenu.getInputMenu(input);
+
+        System.out.println(inputMenu);
+
     }
 }
