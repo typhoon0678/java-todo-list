d557493642becfa8a2410f84d5ce6fc8346ffa73
typhoon0678 <typhoon0678@gmail.com>
2024-07-10 16:40:04 +0900

    view 관련 호출 이동

--- src/main/java/org/homework/controller/TodoController.java
+++ src/main/java/org/homework/controller/TodoController.java
@@ -1,37 +1,80 @@
 package org.homework.controller;
 
 import org.homework.domain.InputMenu;
+import org.homework.domain.Todo;
 import org.homework.service.TodoService;
+import org.homework.view.InputView;
+import org.homework.view.OutputView;
 
 public class TodoController {
 
     private final TodoService todoService = new TodoService();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
 
     InputMenu menu;
+    int todoId;
+    Todo todo;
 
     public void run() {
 
         do {
-            menu = todoService.selectMenu();
+            String menuValue = inputView.selectMenu();
+            menu = InputMenu.getInputMenu(menuValue);
 
             switch (menu) {
                 case ADD:
-                    todoService.addTodo();
+                    String content = inputView.addTodo();
+                    todoId = todoService.addTodo(content);
+
+                    outputView.addTodo(todoId);
                     break;
+
                 case DELETE:
-                    todoService.deleteTodo();
+                    todoId = inputView.deleteTodo();
+                    boolean isDeleted = todoService.deleteTodo(todoId);
+
+                    if (isDeleted) {
+                        outputView.deleteTodo(todoId);
+                    } else {
+                        outputView.getNullTodo();
+                    }
                     break;
+
                 case VIEW:
-                    todoService.viewTodo();
+                    todoId = inputView.getTodo();
+                    todo = todoService.getTodo(todoId);
+
+                    if (todo != null) {
+                        outputView.getTodo(todo);
+                    } else {
+                        outputView.getNullTodo();
+                    }
                     break;
+
                 case UPDATE:
-                    todoService.updateTodo();
+                    todoId = inputView.updateTodo();
+                    todo = todoService.getTodo(todoId);
+
+                    if (todo == null) {
+                        outputView.getNullTodo();
+                        break;
+                    }
+
+                    boolean isUpdated = todoService.updateTodo(todo);
+                    if (isUpdated) {
+                        outputView.updateCompleteTrue(todo);
+                    } else {
+                        outputView.keepCompleteTrue(todo);
+                    }
                     break;
+
                 case EXIT:
-                    todoService.exitMenu();
+                    outputView.exit();
                     break;
+
                 case OTHERWISE:
-                    todoService.invalidInputMenu();
+                    outputView.invalidInput();
             }
 
         } while (!InputMenu.isExit(menu));
--- src/main/java/org/homework/service/TodoService.java
+++ src/main/java/org/homework/service/TodoService.java
@@ -1,70 +1,32 @@
 package org.homework.service;
 
-import org.homework.domain.InputMenu;
 import org.homework.domain.Todo;
 import org.homework.repository.TodoRepository;
-import org.homework.view.InputView;
-import org.homework.view.OutputView;
 
 public class TodoService {
 
-    private final InputView inputView = new InputView();
-    private final OutputView outputView = new OutputView();
-
-    // todoMethod
-
-    public void addTodo() {
-        String content = inputView.addTodo();
-        int todoId = TodoRepository.addTodo(content);
-        outputView.addTodo(todoId);
+    public int addTodo(String content) {
+        return TodoRepository.addTodo(content);
     }
 
-    public void deleteTodo() {
-        int todoId = inputView.deleteTodo();
-        boolean isDeleted = TodoRepository.deleteTodo(todoId);
-
-        if (isDeleted) {
-            outputView.deleteTodo(todoId);
-        } else {
-            outputView.getNullTodo();
-        }
+    public boolean deleteTodo(int todoId) {
+        return TodoRepository.deleteTodo(todoId);
     }
 
-    public void viewTodo() {
-        int todoId = inputView.getTodo();
-
+    public Todo getTodo(int todoId) {
         if (TodoRepository.containsTodo(todoId)) {
-            Todo todo = TodoRepository.getTodo(todoId);
-            outputView.getTodo(todo);
-        } else {
-            outputView.getNullTodo();
+            return TodoRepository.getTodo(todoId);
         }
+
+        return null;
     }
 
-    public void updateTodo() {
-        int todoId = inputView.updateTodo();
-
-        if (TodoRepository.containsTodo(todoId)) {
-            Todo todo = TodoRepository.getTodo(todoId);
-            TodoRepository.updateTodoComplete(todoId);
-            outputView.updateCompleteTrue(todo);
-        } else {
-            outputView.getNullTodo();
+    public boolean updateTodo(Todo todo) {
+        if (!todo.isCompleted()) {
+            TodoRepository.updateTodoComplete(todo.getId());
+            return true;
         }
-    }
 
-    // menuMethod
-
-    public InputMenu selectMenu() {
-        String menuValue = inputView.selectMenu();
-        return InputMenu.getInputMenu(menuValue);
-    }
-
-    public void exitMenu() {
-        outputView.exit();
-    }
-
-    public void invalidInputMenu() {
-        outputView.invalidInput();
+        return false;
     }
 }
